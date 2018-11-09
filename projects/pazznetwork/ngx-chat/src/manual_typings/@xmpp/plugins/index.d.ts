e15bb6c595c3c6e00445ad2fb080e95a7c0d6e53
Fabian Trampusch <fabian.trampusch@freenet.de>
2018-11-09 16:03:37 +0100

    refactor: revert types for 0.3.0

---
+++ projects/pazznetwork/ngx-chat/src/manual_typings/@xmpp/plugins/index.d.ts
@@ -0,0 +1,5 @@
+declare module '@xmpp/plugins/bind';
+declare module '@xmpp/plugins/sasl-plain';
+declare module '@xmpp/plugins/session-establishment';
+declare module '@xmpp/plugins/websocket';
+declare module '@xmpp/plugins';
